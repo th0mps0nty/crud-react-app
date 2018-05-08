ed547d31ac67ba61578a8485daa4c7c6ce8a6d56
Tyler Thompson <th0mps0n.ty@protonmail.com>
2018-05-08 14:51:49 -0400

    finished working CRUD application, using local storage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,99 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import ProductItem from './productItem';
+import AddProduct from './addProduct';
+
+const products = [
+  {
+    name: "iPad",
+    price: 200
+  },
+  {
+    name: "iPhone",
+    price: 600
+  }
+];
+
+localStorage.setItem("products", JSON.stringify(products));
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: JSON.parse(localStorage.getItem("products"))
+    };
+
+    this.onAdd = this.onAdd.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    const products = this.getProducts();
+
+    this.setState({ products });
+  }
+
+  getProducts() {
+    return this.state.products;
+  }
+
+  onAdd(name, price) {
+    const products = this.getProducts();
+
+    products.push({
+      name,
+      price
+    });
+
+    this.setState({ products });
+  }
+
+  onDelete(name) {
+    const products = this.getProducts();
+
+    const filteredProducts = products.filter(product => {
+      return product.name !== name;
+    });
+
+    this.setState({ products: filteredProducts });
+  }
+
+  onEditSubmit(name, price, originalName) {
+    let products = this.getProducts();
+
+    products = products.map(product => {
+      if (product.name === originalName) {
+        product.name = name;
+        product.price = price;
+      }
+
+      return product;
+    });
+
+    this.setState({ products });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Products Manager</h1>
+
+        <AddProduct
+          onAdd={this.onAdd}
+        />
+        {this.state.products.map(product => {
+          return (
+            <ProductItem
+              key={product.name}
+              {...product}
+              onDelete={this.onDelete}
+              onEditSubmit={this.onEditSubmit}
+            />
+          );
+        })}
       </div>
     );
   }
